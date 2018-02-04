db737267c609ab468a764758b488f06853bfe550
prologue <prologue@xiaolingent.com>
2018-02-04 15:18:36 +0900

    Initial Commit

---
+++ Scene-Trans01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by prologue on 2018. 2. 4..
+//  Copyright © 2018년 SQLPRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
